fb77f27422183051346c102eb9bc90dcf6d627c7
E-Svensson <111962443+E-Svensson@users.noreply.github.com>
2022-09-09 10:45:54 +0200

    Add project files.

---
+++ Kappitel 2 uppgift 2.14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace uppgift_2_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skirv in första talet");
+            string tal = Console.ReadLine();
+            Console.WriteLine("Skriv in andra tal"); 
+            string tal2 = Console.ReadLine();
+
+            float tal_siffror = float.Parse(tal);
+            float tal_siffror2 = float.Parse(tal2);
+
+            Console.WriteLine("svaret är" + tal_siffror * tal_siffror2);
+
+
+        }
+    }
+}
